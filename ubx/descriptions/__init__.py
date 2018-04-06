fd04a6475490dfe952ed17041deb1455d666b102
Sebastian Kraemer <basti.kr@gmail.com>
2018-04-06 21:19:41 +0200

    Add nav-sat message description.

--- ubx/descriptions/__init__.py
+++ ubx/descriptions/__init__.py
@@ -1,8 +1,10 @@
 from . import mon_hw
 from . import rxm_rawx
+from . import nav_sat
 
 
 default = [
     mon_hw.description,
     rxm_rawx.description,
+    nav_sat.description
 ]
---
+++ ubx/descriptions/nav_sat.py
@@ -0,0 +1,28 @@
+from ..payload import *
+from ..message import *
+
+
+svs_fields = Fields((
+    ("gnssId", U1),
+    ("svId", U1),
+    ("cno", U1),
+    ("elev", I1),
+    ("azim", I2),
+    ("prRes", I2),
+    ("flags", X4),
+))
+
+payload_description = Fields((
+    ("iTOW", U4),
+    ("version", U1),
+    ("numSvs", U1),
+    ("reserved1", List([U1, U1])),
+    ("meas", Loop(key = "numSvs", description = svs_fields))
+))
+
+description = MessageDescription(
+    name = "RXM-SFRBX",
+    message_class = b"\x01",
+    message_id = b"\x35",
+    payload_description = payload_description
+)
