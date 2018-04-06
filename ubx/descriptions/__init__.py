71e89585f7e495b92d592dfed41c1c1613a5bdb0
Sebastian Kraemer <basti.kr@gmail.com>
2018-04-06 21:31:28 +0200

    Add rxm-sfrbx message description.

--- ubx/descriptions/__init__.py
+++ ubx/descriptions/__init__.py
@@ -1,10 +1,12 @@
 from . import mon_hw
 from . import rxm_rawx
+from . import rxm_sfrbx
 from . import nav_sat
 
 
 default = [
     mon_hw.description,
     rxm_rawx.description,
-    nav_sat.description
+    rxm_sfrbx.description,
+    nav_sat.description,
 ]
---
+++ ubx/descriptions/rxm_sfrbx.py
@@ -0,0 +1,26 @@
+from ..payload import *
+from ..message import *
+
+
+words_fields = Fields(
+    [("dwrd", U4)]
+)
+
+payload_description = Fields((
+    ("gnssId", U1),
+    ("svId", U1),
+    ("reserved1", U1),
+    ("freqId", U1),
+    ("numWords", U1),
+    ("reserved2", U1),
+    ("version", U1),
+    ("reserved3", U1),
+    ("meas", Loop(key = "numWords", description = words_fields))
+))
+
+description = MessageDescription(
+    name = "RXM-SFRBX",
+    message_class = b"\x02",
+    message_id = b"\x13",
+    payload_description = payload_description
+)
