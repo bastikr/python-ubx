c4c0ab9ae0d9bb8dc4ca870de409337290166bfa
Sebastian Kraemer <basti.kr@gmail.com>
2018-04-23 18:15:34 +0200

    More efficient reading from input stream in UBXReader.

--- ubx/reader.py
+++ ubx/reader.py
@@ -41,50 +41,22 @@ class UBXReader:
         if byte != b"\x62":
             raise UBXReaderException("Second syncchar is '{}' instead of '62'".format(codecs.encode(byte, "hex")))
 
-    def read_message_class(self):
-        byte = self.read_checked(1)
-        return byte
-
-    def read_message_id(self):
-        byte = self.read_checked(1)
-        return byte
-
-    def read_length(self):
-        b = self.read_checked(2)
-        return b
-
-    def read_payload(self, length):
-        b = self.read_checked(length)
-        return b
-
-    def read_checksumA(self):
-        byte = self.read_checked(1)
-        return byte
-
-    def read_checksumB(self):
-        byte = self.read_checked(1)
-        return byte
-
     def read_rawmessage(self, seek_syncchars=True):
         if seek_syncchars:
             self.seek_syncchars()
         else:
             self.check_syncchars()
-        byte_message_class = self.read_message_class()
-        byte_message_id = self.read_message_id()
-        byte_length = self.read_length()
+        header = self.read_checked(4)
+        byte_message_class = header[0:1]
+        byte_message_id = header[1:2]
+        byte_length = header[2:4]
+
         length = struct.unpack("<H", byte_length)[0]
-        byte_payload = self.read_payload(length)
-        byte_a = self.read_checksumA()
-        byte_b = self.read_checksumB()
-        try:
-            int_a = struct.unpack("<B", byte_a)[0]
-        except Exception as e:
-            print("Length of byte_a: ", len(byte_a))
-            print("Hex byte_a: ", codecs.encode(byte_a, "hex"))
-            raise e
-        int_b = struct.unpack("<B", byte_b)[0]
+        bytes_payload = self.read_checked(length)
+
+        bytes_checksum = self.read_checked(2)
+        int_a, int_b = struct.unpack("<BB", bytes_checksum)
         checksum_received = checksum.Checksum(int_a, int_b)
-        checksum_calculated = checksum.Checksum.from_bytestrings(byte_message_class, byte_message_id, byte_length, byte_payload)
+        checksum_calculated = checksum.Checksum.from_bytestrings(byte_message_class, byte_message_id, byte_length, bytes_payload)
         checksum_received.check_equal(checksum_calculated)
-        return rawmessage.RawMessage(byte_message_class, byte_message_id, byte_payload)
+        return rawmessage.RawMessage(byte_message_class, byte_message_id, bytes_payload)
