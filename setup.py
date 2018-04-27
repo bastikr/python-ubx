f833ab70631d8da5155ce7466a66267326c23bea
Sebastian Kraemer <basti.kr@gmail.com>
2018-04-27 07:11:27 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(name='python-ubx',
+      version='0.1',
+      description='A python library implementing the binary u-blox UBX protocol.',
+      author='Sebastian Krämer',
+      author_email='basti.kr@gmail.com',
+      url='https://github.com/bastikr/python-ubx',
+      packages=['ubx', 'ubx.descriptions'],
+      scripts=['scripts/ubxstats']
+     )
