0fdda19ac73a934427e68a7fa9a7552bca10100c
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-15 10:34:24 +0200

    Add AID-AOP message description.

---
+++ ubx/descriptions/aid_aop.py
@@ -0,0 +1,25 @@
+from ..payload import *
+from ..message import *
+
+
+payload_description0 = Fields(
+    ("svid", U1),
+)
+
+payload_description1 = Fields(
+    ("gnssId", U1),
+    ("svId", U1),
+    ("reserved1", List(2*[U1])),
+    ("data", List(64*[U1]))
+)
+
+description = MessageDescription(
+    name="AID-AOP",
+    message_class=b"\x0b",
+    message_id=b"\x33",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+        payload_description1
+    )
+)
