d31e72d42507e2f42035d531e2f41e038991d502
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-15 10:10:55 +0200

    Add CFG-MSG message description.

---
+++ ubx/descriptions/cfg_msg.py
@@ -0,0 +1,31 @@
+from ..payload import *
+from ..message import *
+
+
+payload_description0 = Fields(
+    ("msgClass", U1),
+    ("msgID", U1)
+)
+
+payload_description1 = Fields(
+    ("msgClass", U1),
+    ("msgID", U1),
+    ("rate", List(6*[U1]))
+)
+
+payload_description2 = Fields(
+    ("msgClass", U1),
+    ("msgID", U1),
+    ("rate", U1)
+)
+
+description = MessageDescription(
+    name="CFG-MSG",
+    message_class=b"\x06",
+    message_id=b"\x01",
+    payload_description=Options(
+        payload_description0,
+        payload_description1,
+        payload_description2
+    )
+)
