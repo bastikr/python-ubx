a06b17d69de705149e0de58b1cccbd481534a177
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-16 18:26:23 +0200

    Add CFG-PMS message description.

---
+++ ubx/descriptions/cfg_pms.py
@@ -0,0 +1,22 @@
+from ..payload import *
+from ..message import *
+
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("powerSetupValue", U1),
+    ("period", U2),
+    ("onTime", U2),
+    ("reserved1", List(2*[U1]))
+)
+
+
+description = MessageDescription(
+    name="CFG-PMS",
+    message_class=b"\x06",
+    message_id=b"\x86",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
