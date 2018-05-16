164b6aebb605070133b8d3fe8308463619fe61f9
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-16 07:12:35 +0200

    Add CFG-ITFM message description.

---
+++ ubx/descriptions/cfg_itfm.py
@@ -0,0 +1,31 @@
+from ..payload import *
+from ..message import *
+
+
+config = Bitfield(4, entries=[
+    BitfieldEntry("bbThreshold", slice(0, 4)),
+    BitfieldEntry("cwThreshold", slice(4, 9)),
+    BitfieldEntry("algorithmBits", slice(9, 32)),
+    BitfieldEntry("enable", 31)
+])
+
+config2 = Bitfield(4, entries=[
+    BitfieldEntry("generalBits", slice(0, 12)),
+    BitfieldEntry("antSetting", slice(12, 14)),
+    BitfieldEntry("enable2", 14)
+])
+
+payload_description0 = Fields(
+    ("config", config),
+    ("config2", config2),
+)
+
+description = MessageDescription(
+    name="CFG-ITFM",
+    message_class=b"\x06",
+    message_id=b"\x39",
+    payload_description=Options(
+        Empty,
+        payload_description0
+    )
+)
