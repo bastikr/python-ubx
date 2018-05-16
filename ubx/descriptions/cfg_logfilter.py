3364a2536e699a67915fca595c1735556efe83dd
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-16 07:13:04 +0200

    Add CFG-LOGFILTER message description.

---
+++ ubx/descriptions/cfg_logfilter.py
@@ -0,0 +1,28 @@
+from ..payload import *
+from ..message import *
+
+
+flags = Bitfield(1, entries=[
+    BitfieldEntry("recordEnabled", 0),
+    BitfieldEntry("psmOncePerWakupEnabled", 1),
+    BitfieldEntry("applyAllFilterSettings", 2),
+])
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("flags", flags),
+    ("minInterval", U2),
+    ("timeThreshold", U2),
+    ("speedThreshold", U2),
+    ("positionThreshold", U4),
+)
+
+description = MessageDescription(
+    name="CFG-LOGFILTER",
+    message_class=b"\x06",
+    message_id=b"\x47",
+    payload_description=Options(
+        Empty,
+        payload_description0
+    )
+)
