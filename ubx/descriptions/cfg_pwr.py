70dc918e6ec596db99edd75e89b3a90d4fd4582d
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-17 07:10:51 +0200

    Add CFG-PWR message description.

---
+++ ubx/descriptions/cfg_pwr.py
@@ -0,0 +1,19 @@
+from ..payload import *
+from ..message import *
+
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("reserved1", List(3*[U1])),
+    ("state", U4),
+)
+
+description = MessageDescription(
+    name="CFG-PWR",
+    message_class=b"\x06",
+    message_id=b"\x57",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
