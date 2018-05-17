da23e3d16fb7d8e15dcd127cef80cbab946ea466
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-17 07:13:45 +0200

    Add CFG-TMODE2 message description.

---
+++ ubx/descriptions/cfg_tmode2.py
@@ -0,0 +1,30 @@
+from ..payload import *
+from ..message import *
+
+
+flags = Bitfield(2, entries=[
+    BitfieldEntry("lla", 0),
+    BitfieldEntry("altInv", 1),
+])
+
+payload_description0 = Fields(
+    ("timeMode", U1),
+    ("reserved1", U1),
+    ("flags", flags),
+    ("ecefXOrLat", I4),
+    ("ecefYOrLon", I4),
+    ("ecefZOrAlt", I4),
+    ("fixedPosAcc", U4),
+    ("svinMinDur", U4),
+    ("svinAccLim", U4)
+)
+
+description = MessageDescription(
+    name="CFG-TMODE2",
+    message_class=b"\x06",
+    message_id=b"\x3d",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
