b6d41ca629514e8f9c76e9ceb96acaeb607b9c4c
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-17 07:14:05 +0200

    Add CFG-TMODE3 message description.

---
+++ ubx/descriptions/cfg_tmode3.py
@@ -0,0 +1,35 @@
+from ..payload import *
+from ..message import *
+
+
+flags = Bitfield(2, entries=[
+    BitfieldEntry("mode", slice(0, 8)),
+    BitfieldEntry("lla", 8),
+])
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("reserved1", U1),
+    ("flags", flags),
+    ("ecefXOrLat", I4),
+    ("ecefYOrLon", I4),
+    ("ecefZOrAlt", I4),
+    ("ecefXOrLatHP", I1),
+    ("ecefYOrLonHP", I1),
+    ("ecefZOrAltHP", I1),
+    ("reserved2", U1),
+    ("fixedPosAcc", U4),
+    ("svinMinDur", U4),
+    ("svinAccLimit", U4),
+    ("reserved3", List(8*[U1])),
+)
+
+description = MessageDescription(
+    name="CFG-TMODE3",
+    message_class=b"\x06",
+    message_id=b"\x71",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
