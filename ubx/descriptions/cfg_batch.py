d9132263d6a67e2468c480a6368e2eaed0153a46
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-23 18:33:42 +0200

    Add CFG-BATCH message description.

---
+++ ubx/descriptions/cfg_batch.py
@@ -0,0 +1,31 @@
+from ..payload import *
+from ..message import *
+
+
+flags = Bitfield(2, entries=[
+    BitfieldEntry("enable", 0),
+    BitfieldEntry("extraPvt", 2),
+    BitfieldEntry("extraOdo", 3),
+    BitfieldEntry("pioEnable", 5),
+    BitfieldEntry("pioActiveLow", 6)
+
+])
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("flags", flags),
+    ("bufSize", U2),
+    ("notifThrs", U2),
+    ("pioId", U1),
+    ("reserved1", U1)
+)
+
+description = MessageDescription(
+    name="CFG-BATCH",
+    message_class=b"\x06",
+    message_id=b"\x93",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
