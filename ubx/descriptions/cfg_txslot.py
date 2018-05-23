7c238f89c081b30b60308d5123f5acf31bc34dcc
Sebastian Kraemer <basti.kr@gmail.com>
2018-05-23 18:34:49 +0200

    Add CFG-TXSLOT message description.

---
+++ ubx/descriptions/cfg_txslot.py
@@ -0,0 +1,29 @@
+from ..payload import *
+from ..message import *
+
+
+enable = Bitfield(1, entries=[
+    BitfieldEntry("DDC", 0),
+    BitfieldEntry("UART1", 1),
+    BitfieldEntry("UART2", 2),
+    BitfieldEntry("USB", 3),
+    BitfieldEntry("SPI", 4)
+])
+
+payload_description0 = Fields(
+    ("version", U1),
+    ("enable", enable),
+    ("refTp", U1),
+    ("reserved1", U1),
+    ("end", List(4*[U4]))
+)
+
+description = MessageDescription(
+    name="CFG-TXSLOT",
+    message_class=b"\x06",
+    message_id=b"\x53",
+    payload_description=Options(
+        Empty,
+        payload_description0,
+    )
+)
