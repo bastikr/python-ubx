9811d74ba84bb37e383e0397db02e1de7699df45
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-04 16:37:45 +0200

    Better argument checks for hexstring <-> byte conversion functions.

--- ubx/utils.py
+++ ubx/utils.py
@@ -4,8 +4,12 @@ import codecs
 def byte2hexstring(byte):
     if not isinstance(byte, bytes):
         raise TypeError("Expected argument \"byte\" to be of type \"bytes\" but it is of type \"{}\"".format(type(byte)))
+    if len(byte)!=1:
+        raise ValueError("Expected length of the given bytestring to by equal to 1.")
     return codecs.encode(byte, "hex").decode("utf-8")
 
 
 def hexstring2byte(hexstring):
+    if len(hexstring)!=2:
+        raise ValueError("Expected length of the given hexstring to by equal to 2.")
     return codecs.decode(hexstring.encode("utf-8"), "hex")
