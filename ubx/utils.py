ca41d27e993bff42f11b4bdfdf6c4f3a01f66d1c
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-04 15:59:00 +0200

    Implement hex2bytestring funtion.

--- ubx/utils.py
+++ ubx/utils.py
@@ -1,5 +1,11 @@
 import codecs
 
+
 def byte2hexstring(byte):
-    assert isinstance(byte, bytes)
+    if not isinstance(byte, bytes):
+        raise TypeError("Expected argument \"byte\" to be of type \"bytes\" but it is of type \"{}\"".format(type(byte)))
     return codecs.encode(byte, "hex").decode("utf-8")
+
+
+def hexstring2byte(hexstring):
+    return codecs.decode(hexstring.encode("utf-8"), "hex")
