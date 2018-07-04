4a4d68ad00993cabf96a97c5ca067a642c2fa041
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-04 16:04:26 +0200

    Implement functions to obtain class ids by name and byte.

--- ubx/classid.py
+++ ubx/classid.py
@@ -1,3 +1,6 @@
+from . import utils
+
+
 class ClassId:
     def __init__(self, name, classbyte):
         self.name = name
@@ -13,17 +16,38 @@ class ClassId:
         return not self==other
 
 
-NAV = ClassId("NAV", b"\x01")
-RXM = ClassId("RXM", b"\x02")
-INF = ClassId("INF", b"\x04")
-ACK = ClassId("ACK", b"\x05")
-CFG = ClassId("CFG", b"\x06")
-UPD = ClassId("UPD", b"\x09")
-MON = ClassId("MON", b"\x0a")
-AID = ClassId("AID", b"\x0b")
-TIM = ClassId("TIM", b"\x0d")
-ESF = ClassId("ESF", b"\x10")
-MGA = ClassId("MGA", b"\x13")
-LOG = ClassId("LOG", b"\x21")
-SEC = ClassId("SEC", b"\x27")
-HNR = ClassId("HNR", b"\x28")
+ids = [
+    ClassId("NAV", b"\x01"),
+    ClassId("RXM", b"\x02"),
+    ClassId("INF", b"\x04"),
+    ClassId("ACK", b"\x05"),
+    ClassId("CFG", b"\x06"),
+    ClassId("UPD", b"\x09"),
+    ClassId("MON", b"\x0a"),
+    ClassId("AID", b"\x0b"),
+    ClassId("TIM", b"\x0d"),
+    ClassId("ESF", b"\x10"),
+    ClassId("MGA", b"\x13"),
+    ClassId("LOG", b"\x21"),
+    ClassId("SEC", b"\x27"),
+    ClassId("HNR", b"\x28"),
+]
+
+for id in ids:
+    globals()[id.name] = id
+
+def get_by_name(name):
+    for id in ids:
+        if id.name==name:
+            return id
+    raise ValueError("Unknown ClassId of name \"{}\".".format(name))
+
+def get_by_byte(byte, defaultname=None):
+    if not isinstance(byte, bytes):
+        raise TypeError("Expected byte argument to be of type \"bytes\" but it is \"{}\".".format(type(byte)))
+    for id in ids:
+        if id.classbyte==byte:
+            return id
+    if defaultname is None:
+        raise ValueError("Unknown ClassId with byte \"0x{}\".".format(utils.byte2hexstring(byte)))
+    return ClassId(defaultname, byte)
