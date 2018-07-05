b362276810f405bc8ff554625f7747dd92613ae9
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-05 06:36:11 +0200

    Docstrings for utils functions.

--- ubx/utils.py
+++ ubx/utils.py
@@ -2,6 +2,9 @@ import codecs
 
 
 def byte2hexstring(byte):
+    """
+    Return a string containing the hex representation of a given byte.
+    """
     if not isinstance(byte, bytes):
         raise TypeError("Expected argument \"byte\" to be of type \"bytes\" but it is of type \"{}\"".format(type(byte)))
     if len(byte)!=1:
@@ -10,6 +13,9 @@ def byte2hexstring(byte):
 
 
 def hexstring2byte(hexstring):
+    """
+    Return a byte corresponding to the given hex string.
+    """
     if len(hexstring)!=2:
         raise ValueError("Expected length of the given hexstring to by equal to 2.")
     return codecs.decode(hexstring.encode("utf-8"), "hex")
