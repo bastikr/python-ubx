85409c672ca9b50c30b031ff74a687265e838e98
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-05 07:09:56 +0200

    Docstrings for classid.py

--- ubx/classid.py
+++ ubx/classid.py
@@ -1,7 +1,12 @@
+"""UBX message classes."""
+
 from . import utils
 
 
 class ClassId:
+    """
+    Simple class storing the name and byte of a UBX message class.
+    """
     def __init__(self, name, byte):
         self.name = name
         self.byte = byte
@@ -32,22 +37,33 @@ ids = [
     ClassId("SEC", b"\x27"),
     ClassId("HNR", b"\x28"),
 ]
+"""UBX message classes."""
 
+
+# Insert message classes into module namespace
 for id in ids:
     globals()[id.name] = id
 
+
 def get_by_name(name):
+    """Obtain the message class with the given name.
+
+    Raises a ValueError if no message class matches the given name.
+    """
     for id in ids:
         if id.name==name:
             return id
     raise ValueError("Unknown ClassId of name \"{}\".".format(name))
 
-def get_by_byte(byte, defaultname=None):
+
+def get_by_byte(byte):
+    """Obtain the message class with the given byte.
+
+    Raises a ValueError if no message class matches the given byte.
+    """
     if not isinstance(byte, bytes):
         raise TypeError("Expected byte argument to be of type \"bytes\" but it is \"{}\".".format(type(byte)))
     for id in ids:
         if id.byte==byte:
             return id
-    if defaultname is None:
-        raise ValueError("Unknown ClassId with byte \"0x{}\".".format(utils.byte2hexstring(byte)))
-    return ClassId(defaultname, byte)
+    raise ValueError("Unknown ClassId with byte \"0x{}\".".format(utils.byte2hexstring(byte)))
