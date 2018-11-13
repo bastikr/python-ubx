eb12056091e1551ffc43396ef80540b3a59bac8c
Sebastian Kraemer <basti.kr@gmail.com>
2018-11-13 18:39:21 +0100

    Allow specification of chars encoding.

--- ubx/payload.py
+++ ubx/payload.py
@@ -1,4 +1,5 @@
 import struct
+import codecs
 from collections import OrderedDict
 
 import bitarray
@@ -182,8 +183,9 @@ X8 = Bitfield(8)
 
 
 class Chars:
-    def __init__(self, bytesize):
+    def __init__(self, bytesize, encoding="iso-8859-1"):
         self.bytesize = bytesize
+        self.encoding = codecs.lookup(encoding)
 
     def parse(self, buffer, context=None):
         buffer.check_bytesize(self.bytesize, "Chars", context)
@@ -191,10 +193,10 @@ class Chars:
             bytestring = buffer.read(buffer.remaining_bytesize)
         else:
             bytestring = buffer.read(self.bytesize)
-        return bytestring.decode("iso-8859-1")
+        return bytestring.decode(self.encoding)
 
     def serialize(self, content, context=None):
-        return content.encode("iso-8859-1")
+        return content.encode(self.encoding)
 
     def __str__(self):
         return "Chars({})".format(self.bytesize)
